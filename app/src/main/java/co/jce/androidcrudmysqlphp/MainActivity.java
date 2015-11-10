11ba7827a61c8bf1f97376aae332d3481e4ff289
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2015-11-09 19:24:51 -0500

    (C) Modifica el nombre de los botones de la GUI.

--- app/src/main/java/co/jce/androidcrudmysqlphp/MainActivity.java
+++ app/src/main/java/co/jce/androidcrudmysqlphp/MainActivity.java
@@ -1,13 +1,31 @@
 package co.jce.androidcrudmysqlphp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //-> Define los componentes
+    private EditText etNombres,
+                     etApellidos,
+                     etCedula,
+                     etCargo,
+                     etCorreoElectronico;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+    
+    private void init() {
+        //-> Accedemos a los compomentes del Activity
+        etNombres           = ( EditText ) findViewById( R .id .etNombres );
+        etApellidos         = ( EditText ) findViewById( R .id .etApellidos );
+        etCedula            = ( EditText ) findViewById( R .id .etCedula );
+        etCargo             = ( EditText ) findViewById( R .id .etCargo );
+        etCorreoElectronico = ( EditText ) findViewById( R .id .etCorreoElectronico);
     }
 }
