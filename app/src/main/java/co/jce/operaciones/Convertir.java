1e16202afc6e8badd7e4ea911253ecee2dc454f6
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2015-11-12 15:50:34 -0500

    (F) Agrega funcionalidad general a la aplicación
    Crea la clase Convertir que proporciona las siguientes
    funcionalidades:
    
     - cadenaEnArrayListDeArray(): método que recibe como
       parámetro una cadena datos separados por comas (,)
       y dobles puntos (::) representando registros de una
       base de datos. Este pemite extraer dichos datos para
       luego retornar un ArrarList con un Array de tipo
       String (ArrayList<String[]>) en cada uno de sus campos.
     - arrayListDeArrayEnArray(): método que recibe como
       parámentro un ArrarList con un Array de tipo
       String (ArrayList<String[]>) y un entero que indicará
       el campo del String[] que se desea extraer del
       ArrayList retornando un String[] con cada uno de los
       valores extraidos.

---
+++ app/src/main/java/co/jce/operaciones/Convertir.java
@@ -0,0 +1,60 @@
+package co.jce.operaciones;
+
+import android.util.Log;
+
+import java.util.ArrayList;
+
+/**
+ * Created by jcarlosj on 11/11/15.
+ */
+public class Convertir {
+
+    public static ArrayList<String[]> cadenaEnArrayListDeArray( String datos ) {
+
+        //-> Segmenta y extrae cada registro y lo agrega a un "Array" de tipo "String"
+        int agrega = 0;                         //: Contando veces que se registro en el ArrayList
+        String[] registro, campos = null;
+        ArrayList alDatos;
+        alDatos = new ArrayList<String[]>();
+
+        //-> Divide cada que encuentra un :: (Siempre genera una división adicional)
+        registro = datos.split("::");       //: El último registro no trae nada, por eso se reduce en 1.
+
+        //-> Recorre el "Array" registro
+        for( int i = 0; i < registro .length - 1; i++ ) {           //: Reducimos en uno el tamaño del Array.
+            /*-> Segmenta y extrae cada campo de cada uno de los registros
+                 y lo agrega a un "Array" de tipo "String"                  */
+            campos = registro[ i ] .split(",");
+            //-(T) System .out .println( "Array (registro): " + registro[ i ] );
+            alDatos .add( campos );
+            agrega++;
+        }
+        Log .i( "alDatos", "agrego " + agrega + " registros" );
+
+        //-> Imprime "Array" campos para verificar su contenido (PRUEBA)
+        for( int i = 0; i < campos .length; i++ ) {
+            System .out.println("Convertir campos[" + i + "]: " + campos[ i ] );
+        }
+
+        return alDatos;
+    }
+
+    public static String[] arrayListDeArrayEnArray( ArrayList<String[]> datos, int filtro ) {
+
+        String aDatos[] = new String[ datos.size() ];
+
+        Log .i( "Longitud", "filas    :" + datos.size() );
+        Log .i( "Longitud", "columnas :" + datos.get(0) .length );
+
+        for( int i = 0; i < datos.size(); i++ ) {
+            for( int j = 0; j < datos.get( 0 ).length; j++ ) {
+                //System .out .println( datos.get( i )[ j ] );
+                aDatos[ i ] = datos.get( i )[ filtro ];
+
+            }
+        }
+
+        return aDatos;
+    }
+
+}
